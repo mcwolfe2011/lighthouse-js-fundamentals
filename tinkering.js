1c1dbc2a8f9bdb5f2766732b0f2efc898907d0ba
Monica Wolfe <mcwolfe2011@gmail.com>
2023-02-11 20:45:36 -0800

    Adding tinkering.js file.

---
+++ tinkering.js
@@ -0,0 +1,13 @@
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// const result = multiply(2, 4);
+
+// console.log(result)
+
+
+function numSum(a, b) {
+  return a + b;
+}
+console.log(numSum(20,30))
