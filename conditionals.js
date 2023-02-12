d0954441e4cc3161390312a807013e84b9d72240
Monica Wolfe <mcwolfe2011@gmail.com>
2023-02-11 20:40:31 -0800

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -22,3 +22,16 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//Logical Operators
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Leave your umbrella at home!");
+}
