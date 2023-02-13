60396c9a81ad34d2e831ee1681bc2fe45a791dec
Monica Wolfe <mcwolfe2011@gmail.com>
2023-02-12 20:17:45 -0800

    Added whichSchool function

--- conditionals.js
+++ conditionals.js
@@ -35,3 +35,20 @@ if (isCitizen && age > 18) {
 if (temperature < -40 || temperature > 40) {
   console.log("Leave your umbrella at home!");
 }
+
+function whichSchool(age) {
+  if (age < 13) {
+    return ("Elementary School");
+  } else if (age >= 13 && age <= 18) {
+    return ("Secondary School");
+  } else {
+    return ("Lighthouse Labs");
+  }
+}
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
